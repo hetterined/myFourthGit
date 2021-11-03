4ec9325587a64169f884463ca30b0107a8acd487
hetterined <hetterined@gmail.com>
2021-11-03 11:46:46 +0400

    added scripts

---
+++ scripts/script.js
@@ -0,0 +1,66 @@
+var = x; 
+var = y;
+var = z;
+
+if (x > y && x > z){
+  console.log(x)
+} else if (y > z){
+  console.log(y)
+} else {
+  console.log(z)
+}
+
+var John = 92;
+var David = 84;
+var Kate = 55;
+var Chris = 65;
+
+
+if (John <= 100 && John >= 90){
+  console.log('A');
+} else if (John <= 90 && John >= 80){
+  console.log ('B');
+} else if (John <= 79 && John >= 70){
+  console.log('C');
+} else if (John <= 69 && John >= 60){
+  console.log ('D');
+} else if (John <= 59 ){
+  console.log('F')
+}
+
+if (David <= 100 && David >= 90){
+  console.log('A');
+} else if (David <= 90 && David >= 80){
+  console.log ('B');
+} else if (David <= 79 && David >= 70){
+  console.log('C');
+} else if (David <= 69 && David >= 60){
+  console.log ('D');
+} else if (David <= 59 ){
+  console.log('F')
+}
+
+if (Kate <= 100 && Kate >= 90){
+  console.log('A');
+} else if (Kate <= 90 && Kate >= 80){
+  console.log ('B');
+} else if (Kate <= 79 && Kate >= 70){
+  console.log('C');
+} else if (Kate <= 69 && Kate >= 60){
+  console.log ('D');
+} else if (Kate <= 59 ){
+  console.log('F')
+}
+
+
+if (Chris <= 100 && Chris >= 90){
+  console.log('A');
+} else if (Chris <= 90 && Chris >= 80){
+  console.log ('B');
+} else if (Chris <= 79 && Chris >= 70){
+  console.log('C');
+} else if (Chris <= 69 && Chris >= 60){
+  console.log ('D');
+} else if (Chris <= 59 ){
+  console.log('F')
+}
